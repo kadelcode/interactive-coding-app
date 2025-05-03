cae492b6ec1295708700a2ce02d4bbc080abbfbf
kadelcode <kadelcode@gmail.com>
2025-05-03 13:05:04 +0100

    Configure images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ui-avatars.com", // Allow images from ui-avatars.com
+      },
+    ]
+  }
 };
 
 export default nextConfig;
